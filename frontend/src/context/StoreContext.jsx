f723c58dd410ec056aa736005fedec3a840bff2a
Andi <ciliandimarian@gmail.com>
2025-03-23 22:02:24 +0200

    implemented "addToCart" and "removeFromCart" functionalities, implemented a filter for every meal cateogry and added a footer

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,21 +1,49 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    
-    const contextValue = {
-food_list
- }
+  const [cartItems, setCartItems] = useState({});
 
-return(
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: 1,
+      }));
+    } else {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: prev[itemId] + 1,
+      }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] - 1,
+    }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
     <StoreContext.Provider value={contextValue}>
-           {props.children}
+      {props.children}
     </StoreContext.Provider>
-   
-)
-
-}
+  );
+};
 
 export default StoreContextProvider;
